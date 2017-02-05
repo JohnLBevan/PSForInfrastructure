2ea7b6a7d92537b8000b768e3590d6d8f8d1b31e
John Bevan <John.Bevan@ClearChannel.co.uk>
2017-02-05 23:48:44 +0000

    #4 - Include Server Description

--- PSForInfrastructure/Get-ServerInventory.ps1
+++ PSForInfrastructure/Get-ServerInventory.ps1
@@ -6,6 +6,22 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 pushd $dir
  
+
+function Get-ServerDescription {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[string]$ComputerName
+	)
+	process {
+		#based on script by Twon of An: https://community.spiceworks.com/topic/525554-powershell-question-retrieve-computer-description
+		$Reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine',$ComputerName)
+		$RegKey= $Reg.OpenSubKey("SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters")
+		$RegKey.GetValue("srvcomment")
+	}
+}
+
+
 [System.Collections.ArrayList]$sysCollection = New-Object System.Collections.ArrayList($null)
   
 foreach ($server in (Get-Content $serverList))
@@ -52,6 +68,8 @@ foreach ($server in (Get-Content $serverList))
 		$si.AllDrivesSize="$([string]([System.Math]::Round($totalSize/1gb,2))) GB"
 		$si.AllDrivesFree="$([string]([System.Math]::Round($totalFree/1gb,2))) GB"
 		$si.AllDrivesUsed="$([string]([System.Math]::Round(($totalSize - $totalFree)/1gb,2))) GB"
+
+		$si.Description = Get-ServerDescription -ComputerName $server
     }
     catch [Exception]
     {
@@ -70,6 +88,6 @@ foreach ($server in (Get-Content $serverList))
 }
   
 $sysCollection `
-    | select-object Server,TotMem,OSName,ServicePack,Arch,Processors,Cores,Manufacturer,Model,BiosDesc,BiosVer,BiosSerial,AllDrivesSize,AllDrivesFree,AllDrivesUsed,DriveA,DriveB,DriveC,DriveD,DriveE,DriveF,DriveG,DriveH,DriveI,DriveJ,DriveK,DriveL,DriveM,DriveN,DriveO,DriveP,DriveQ,DriveR,DriveS,DriveT,DriveU,DriveV,DriveW,DriveX,DriveY,DriveZ,DriveAFree,DriveBFree,DriveCFree,DriveDFree,DriveEFree,DriveFFree,DriveGFree,DriveHFree,DriveIFree,DriveJFree,DriveKFree,DriveLFree,DriveMFree,DriveNFree,DriveOFree,DrivePFree,DriveQFree,DriveRFree,DriveSFree,DriveTFree,DriveUFree,DriveVFree,DriveWFree,DriveXFree,DriveYFree,DriveZFree,DriveAUsed,DriveBUsed,DriveCUsed,DriveDUsed,DriveEUsed,DriveFUsed,DriveGUsed,DriveHUsed,DriveIUsed,DriveJUsed,DriveKUsed,DriveLUsed,DriveMUsed,DriveNUsed,DriveOUsed,DrivePUsed,DriveQUsed,DriveRUsed,DriveSUsed,DriveTUsed,DriveUUsed,DriveVUsed,DriveWUsed,DriveXUsed,DriveYUsed,DriveZUsed,ErrorMessage,ErrorItem `
+    | select-object Server,Description,TotMem,OSName,ServicePack,Arch,Processors,Cores,Manufacturer,Model,BiosDesc,BiosVer,BiosSerial,AllDrivesSize,AllDrivesFree,AllDrivesUsed,DriveA,DriveB,DriveC,DriveD,DriveE,DriveF,DriveG,DriveH,DriveI,DriveJ,DriveK,DriveL,DriveM,DriveN,DriveO,DriveP,DriveQ,DriveR,DriveS,DriveT,DriveU,DriveV,DriveW,DriveX,DriveY,DriveZ,DriveAFree,DriveBFree,DriveCFree,DriveDFree,DriveEFree,DriveFFree,DriveGFree,DriveHFree,DriveIFree,DriveJFree,DriveKFree,DriveLFree,DriveMFree,DriveNFree,DriveOFree,DrivePFree,DriveQFree,DriveRFree,DriveSFree,DriveTFree,DriveUFree,DriveVFree,DriveWFree,DriveXFree,DriveYFree,DriveZFree,DriveAUsed,DriveBUsed,DriveCUsed,DriveDUsed,DriveEUsed,DriveFUsed,DriveGUsed,DriveHUsed,DriveIUsed,DriveJUsed,DriveKUsed,DriveLUsed,DriveMUsed,DriveNUsed,DriveOUsed,DrivePUsed,DriveQUsed,DriveRUsed,DriveSUsed,DriveTUsed,DriveUUsed,DriveVUsed,DriveWUsed,DriveXUsed,DriveYUsed,DriveZUsed,ErrorMessage,ErrorItem `
     | sort -Property Server `
     | Export-CSV -path $outputCSV -NoTypeInformation
